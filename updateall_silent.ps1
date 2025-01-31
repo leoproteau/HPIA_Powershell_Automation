0089b823a9ae7838c8501a95c311fefca7cb03b9
leoproteau <111393553+leoproteau@users.noreply.github.com>
2025-01-31 13:39:44 -0500

    Create updateall_silent.ps1

---
+++ updateall_silent.ps1
@@ -0,0 +1,2 @@
+cd C:\SWSetup\SP154530
+.\HPImageAssistant.exe /Operation:Analyze /Category:All /selection:All /action:install /silent /reportFolder:c:\temp\HPIA\Report /softpaqdownloadfolder:c:\temp\HPIA\download
